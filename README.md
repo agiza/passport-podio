4e5f4f830fe409f2d3e54730ac43e4fdcf0c8faa
Andrew Chilton <chilts@appsattic.com>
2013-02-18 23:52:42 +1300

    First version of passport-podio

---
+++ README.md
@@ -0,0 +1,6 @@
+# Passport-Podio #
+
+[Passport](http://passportjs.org/) strategy for authenticating with [Podio](http://www.podio.com/) using OAuth 2.0.
+
+(Ends)
+
---
+++ passport-podio.js
@@ -0,0 +1,96 @@
+/**
+ * Module dependencies.
+ */
+var util = require('util')
+  , OAuth2Strategy = require('passport-oauth').OAuth2Strategy
+  , InternalOAuthError = require('passport-oauth').InternalOAuthError;
+
+
+/**
+ * `Strategy` constructor.
+ *
+ * The Google authentication strategy authenticates requests by delegating to
+ * Google using the OAuth 2.0 protocol.
+ *
+ * Applications must supply a `verify` callback which accepts an `accessToken`,
+ * `refreshToken` and service-specific `profile`, and then calls the `done`
+ * callback supplying a `user`, which should be set to `false` if the
+ * credentials are not valid.  If an exception occured, `err` should be set.
+ *
+ * Options:
+ *   - `clientID`      your Google application's client id
+ *   - `clientSecret`  your Google application's client secret
+ *   - `callbackURL`   URL to which Google will redirect the user after granting authorization
+ *
+ * Examples:
+ *
+ *     passport.use(new GoogleStrategy({
+ *         clientID: '123-456-789',
+ *         clientSecret: 'shhh-its-a-secret'
+ *         callbackURL: 'https://www.example.net/auth/google/callback'
+ *       },
+ *       function(accessToken, refreshToken, profile, done) {
+ *         User.findOrCreate(..., function (err, user) {
+ *           done(err, user);
+ *         });
+ *       }
+ *     ));
+ *
+ * @param {Object} options
+ * @param {Function} verify
+ * @api public
+ */
+function Strategy(options, verify) {
+  options = options || {};
+  options.authorizationURL = options.authorizationURL || 'https://podio.com/oauth/authorize';
+  options.tokenURL = options.tokenURL || 'https://podio.com/oauth/token';
+  
+  OAuth2Strategy.call(this, options, verify);
+  this.name = 'podio';
+}
+
+/**
+ * Inherit from `OAuth2Strategy`.
+ */
+util.inherits(Strategy, OAuth2Strategy);
+
+
+/**
+ * Retrieve user profile from Podio.
+ *
+ * This function constructs a normalized profile, with the following properties:
+ *
+ *   - `provider`         always set to `podio`
+ *   - `id`
+ *   - `username`
+ *   - `displayName`
+ *
+ * @param {String} accessToken
+ * @param {Function} done
+ * @api protected
+ */
+Strategy.prototype.userProfile = function(accessToken, done) {
+  this._oauth2.get('https://api.podio.com:443/user/profile/', accessToken, function (err, body, res) {
+    if (err) { return done(new InternalOAuthError('failed to fetch user profile', err)); }
+
+    try {
+      var json = JSON.parse(body);
+
+      var profile = { provider: 'podio' };
+      profile.id = json.user_id;
+      profile.displayName = json.name;
+      profile.emails = [{ value: json.mail[0] }];
+
+      profile._raw = body;
+
+      done(null, profile);
+    } catch(e) {
+      done(e);
+    }
+  });
+}
+
+/**
+ * Expose `Strategy`.
+ */
+module.exports = Strategy;
